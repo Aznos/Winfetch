29ead411cbc0470a609e7eea2c692f52414666d7
Pigioty <Pigiotyreal@gmail.com>
2023-04-01 21:58:23 -0400

    CPU Architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <windows.h>
+#include <tchar.h>
+
+void cpuInfo() {
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+
+    std::cout << "CPU Information:\n";
+    std::cout << "  Processor Architecture: ";
+
+    switch(sysinfo.wProcessorArchitecture) {
+        case PROCESSOR_ARCHITECTURE_AMD64:
+            std::cout << "x64\n";
+            break;
+        case PROCESSOR_ARCHITECTURE_INTEL:
+            std::cout << "x86\n";
+            break;
+        case PROCESSOR_ARCHITECTURE_ARM:
+            std::cout << "ARM\n";
+            break;
+        default:
+            std::cout << "Unknown\n";
+            break;
+    }
+}
 
 int main() {
-    std::cout << "Hello world!";
+    std::cout << "Winfetch v0.0.1:\n";
+
+    cpuInfo();
 
     return 0;
 }
