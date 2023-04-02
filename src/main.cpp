acbcd8985820589d758a28152aa05bb80f36ffcb
Pigioty <Pigiotyreal@gmail.com>
2023-04-02 13:42:09 -0400

    Total, used, and available ram

--- src/main.cpp
+++ src/main.cpp
@@ -90,10 +90,26 @@ void osInfo() {
     }
 }
 
+void ramInfo() {
+    std::cout << "RAM Information:\n";
+
+    MEMORYSTATUSEX memoryStatus;
+    memoryStatus.dwLength = sizeof(memoryStatus);
+    GlobalMemoryStatusEx(&memoryStatus);
+
+    DWORDLONG totalram = memoryStatus.ullTotalPhys;
+    DWORDLONG usedram = memoryStatus.ullTotalPhys - memoryStatus.ullAvailPhys;
+    double percentused = (double)usedram / (double)totalram * 100.0;
+    std::cout << "  Total RAM: " << totalram / (1024 * 1024) << "MB\n";
+    std::cout << "  Used RAM: " << std::fixed << std::setprecision(2) << usedram / (1024 * 1024) << "MB (" << percentused << "%)\n";
+    std::cout << "  Available RAM: " << memoryStatus.ullAvailPhys / (1024 * 1024) << "MB\n";
+}
+
 int main() {
     std::cout << "Winfetch v0.0.1\n";
 
     cpuInfo();
+    ramInfo();
     osInfo();
 
     return 0;
