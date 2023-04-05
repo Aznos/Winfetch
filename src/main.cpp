2746b7cdf2044b12ff81e9bde9fae1ac801ea8ab
Pigioty <Pigiotyreal@gmail.com>
2023-04-05 17:18:45 -0400

    Disk info

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <intrin.h>
 #include <iomanip>
+#include <vector>
 
 SYSTEM_INFO sysinfo;
 
@@ -121,12 +122,59 @@ void gpuInfo() {
     }
 }
 
+void diskInfo() {
+    std::cout << "Disk Information:\n";
+
+    DWORD drives = GetLogicalDrives();
+    std::vector<char> buffer(MAX_PATH);
+
+    for(char letter = 'A'; letter <= 'Z'; letter++) {
+        if(drives & 1) {
+            std::string root = "";
+            root += letter;
+            root += ":\\";
+            UINT type = GetDriveType(root.c_str());
+            if(type == DRIVE_FIXED || type == DRIVE_REMOVABLE || type == DRIVE_REMOTE) {
+                ULARGE_INTEGER freeSpace, totalSpace, totalFreeSpace;
+                if(GetDiskFreeSpaceEx(root.c_str(), &freeSpace, &totalSpace, &totalFreeSpace)) {
+                    std::cout << "  Drive " << letter << ":\n";
+                    std::cout << "      Type: ";
+                    switch(type) {
+                        case DRIVE_FIXED:
+                            std::cout << "Fixed\n";
+                            break;
+                        case DRIVE_REMOVABLE:
+                            std::cout << "Removable\n";
+                            break;
+                        case DRIVE_REMOTE:
+                            std::cout << "Remote\n";
+                            break;
+                        default:
+                            std::cout << "Unknown\n";
+                            break;
+                    }
+                    std::cout << "      File System: ";
+                    if(GetVolumeInformation(root.c_str(), &buffer[0], MAX_PATH, NULL, NULL, NULL, NULL, 0)) {
+                        std::cout << &buffer[0] << "\n";
+                    } else {
+                        std::cout << "Unknown\n";
+                    }
+                    std::cout << "      Total Space: " << totalSpace.QuadPart / (1024 * 1024) << "MB\n";
+                    std::cout << "      Free Space: " << freeSpace.QuadPart / (1024 * 1024) << "MB\n";
+                }
+            }
+        }
+        drives >>= 1;
+    }
+}
+
 int main() {
     std::cout << "Winfetch v0.0.1\n";
 
     cpuInfo();
     ramInfo();
     gpuInfo();
+    diskInfo();
     osInfo();
 
     return 0;
