4f4396eeb50c9511d8b6d7a422fe3753541245f8
Pigioty <Pigiotyreal@gmail.com>
2023-04-06 07:48:00 -0400

    Check for graphics card

--- src/main.cpp
+++ src/main.cpp
@@ -120,6 +120,12 @@ void gpuInfo() {
     std::cout << "GPU Information:\n";
 
     IDirect3D9* d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+    if(!d3d) {
+        std::cout << "  No graphics card found\n";
+        return;
+    }
+
     D3DADAPTER_IDENTIFIER9 adapter;
 
     if(d3d->GetAdapterIdentifier(D3DADAPTER_DEFAULT, 0, &adapter) == D3D_OK) {
